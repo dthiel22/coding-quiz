d0e63b4a520baf48af225fd114c2d10037d9c839
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-09-30 17:50:27 -0700

    text content now changes with variables, ready for functions and loops

--- assets/script.js
+++ assets/script.js
@@ -7,13 +7,25 @@ var answersArr1 = [];
 var answersArr2 = [];
 var answersArr3 = [];
 
-//vars to grab that specific class
-var startBtn = document.querySelector("#start-game");
-var questionPlace = document.querySelector("#question-title")
-var question1 = 0;
+questionSelect = questionArr[0]
+answerSelect1 = answersArr0[0]
+answerSelect2 = answersArr0[1]
+answerSelect3 = answersArr0[2]
+answerSelect4 = answersArr0[3]
 
-questionPlace.textContent = questionPicked
-console.log(questionPlace)
+//vars to grab that specific class
+var questionPlace = document.querySelector("#question-title")
+var answerPlace1 = document.querySelector("#answer1")
+var answerPlace2 = document.querySelector("#answer2")
+var answerPlace3 = document.querySelector("#answer3")
+var answerPlace4 = document.querySelector("#answer4")
+
+// vars to grab select, change text to var value
+questionPlace.textContent = questionSelect
+answerPlace1.textContent = answerSelect1
+answerPlace2.textContent = answerSelect2
+answerPlace3.textContent = answerSelect3
+answerPlace4.textContent = answerSelect4
 
 //need a timer to display in the top right of the page
 //original plan is to make divs called question#
