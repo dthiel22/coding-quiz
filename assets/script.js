37c2e005bd6a635ac0d36dd914fdc18d206d5669
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-10-04 12:22:28 -0700

    quiz functions properly and items are stored in array, need to make highscores a differnt page and also mark down points for incorrect answers

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 console.log("linked");
 
 //each questionArr holds each question. answersArr hold all the answers
-var questionPack1 = ["A", "1", "2", "3", "4"];
-var questionPack2 = ["B", "11", "22", "33", "44"];
-var questionPack3 = ["C", "111", "222", "333", "444"];
-var questionPack4 = ["D", "1111", "2222", "3333", "4444"];
+var questionPack1 = ["The _________ value stacks the flex items vertically (from top to bottom)", "column", "row", "wrap", "direction"];
+var questionPack2 = ["The <a> tag defines a ________", "color", "image", "hyperlink", "class"];
+var questionPack3 = ["A JavaScript _______ is a block of code designed to perform a particular task.", "object", "array", "classes", "function"];
+var questionPack4 = ["An _______ is a special variable, which can hold more than one value", "number", "array", "value", "string"];
 var combineSubmit = {
     name: "", time: ""
 };
@@ -51,8 +51,7 @@ startBtn.addEventListener("click", function () {
     timer = setInterval(function () {
         timeLeft--;
         timeLeftSpan.textContent = timeLeft;
-        // if time runs out end game as loss
-        if (timeLeft === 0) {
+        if (timeLeft === 0 || timeLeft < 0) {
             clearInterval(timer);
             isPlaying = false;
             displayForm()
@@ -73,66 +72,54 @@ startBtn.addEventListener("click", function () {
 //click on question to see if "true"
 answerHolder.addEventListener("click", function (event) {
     // once game is started, listen for mouseClick
-    if (isPlaying) {
-        var mouseClick = event.target.textContent;
+    var mouseClick = event.target.textContent;
+    console.log(mouseClick)
+    if (questionSelect === questionPack1[0] && mouseClick === "column") {
         console.log(mouseClick)
-        // if answer is correct, console.log you selected right
-        questionSelect = questionPack1[0]
-        answerSelect1 = questionPack1[1]
-        answerSelect2 = questionPack1[2]
-        answerSelect3 = questionPack1[3]
-        answerSelect4 = questionPack1[4]
+        console.log("you selected right")
+        questionSelect = questionPack2[0]
+        answerSelect1 = questionPack2[1]
+        answerSelect2 = questionPack2[2]
+        answerSelect3 = questionPack2[3]
+        answerSelect4 = questionPack2[4]
         questionPlace.textContent = questionSelect
         answerPlace1.textContent = answerSelect1
         answerPlace2.textContent = answerSelect2
         answerPlace3.textContent = answerSelect3
-        answerPlace4.textContent = answerSelect4
-        if (questionSelect === questionPack1[0] && mouseClick.includes("1")) {
-            console.log("you selected right")
-            questionSelect = questionPack2[0]
-            answerSelect1 = questionPack2[1]
-            answerSelect2 = questionPack2[2]
-            answerSelect3 = questionPack2[3]
-            answerSelect4 = questionPack2[4]
-            questionPlace.textContent = questionSelect
-            answerPlace1.textContent = answerSelect1
-            answerPlace2.textContent = answerSelect2
-            answerPlace3.textContent = answerSelect3
-            answerPlace4.textContent = answerSelect4;
-        }
+        answerPlace4.textContent = answerSelect4;
+    }
 
-        if (questionSelect === questionPack2[0] && mouseClick.includes("11")) {
-            console.log("you selected right")
-            questionSelect = questionPack3[0]
-            answerSelect1 = questionPack3[1]
-            answerSelect2 = questionPack3[2]
-            answerSelect3 = questionPack3[3]
-            answerSelect4 = questionPack3[4]
-            questionPlace.textContent = questionSelect
-            answerPlace1.textContent = answerSelect1
-            answerPlace2.textContent = answerSelect2
-            answerPlace3.textContent = answerSelect3
-            answerPlace4.textContent = answerSelect4;
-        }
+    if (questionSelect === questionPack2[0] && mouseClick === "hyperlink") {
+        console.log("you selected right")
+        questionSelect = questionPack3[0]
+        answerSelect1 = questionPack3[1]
+        answerSelect2 = questionPack3[2]
+        answerSelect3 = questionPack3[3]
+        answerSelect4 = questionPack3[4]
+        questionPlace.textContent = questionSelect
+        answerPlace1.textContent = answerSelect1
+        answerPlace2.textContent = answerSelect2
+        answerPlace3.textContent = answerSelect3
+        answerPlace4.textContent = answerSelect4;
+    }
 
-        if (questionSelect === questionPack3[0] && mouseClick.includes("111")) {
-            console.log("you selected right")
-            questionSelect = questionPack4[0]
-            answerSelect1 = questionPack4[1]
-            answerSelect2 = questionPack4[2]
-            answerSelect3 = questionPack4[3]
-            answerSelect4 = questionPack4[4]
-            questionPlace.textContent = questionSelect
-            answerPlace1.textContent = answerSelect1
-            answerPlace2.textContent = answerSelect2
-            answerPlace3.textContent = answerSelect3
-            answerPlace4.textContent = answerSelect4;
-        }
+    if (questionSelect === questionPack3[0] && mouseClick === "function") {
+        console.log("you selected right")
+        questionSelect = questionPack4[0]
+        answerSelect1 = questionPack4[1]
+        answerSelect2 = questionPack4[2]
+        answerSelect3 = questionPack4[3]
+        answerSelect4 = questionPack4[4]
+        questionPlace.textContent = questionSelect
+        answerPlace1.textContent = answerSelect1
+        answerPlace2.textContent = answerSelect2
+        answerPlace3.textContent = answerSelect3
+        answerPlace4.textContent = answerSelect4;
+    }
 
-        if (questionSelect === questionPack4[0] && mouseClick.includes("1111")) {
-            clearInterval(timer)
-            displayForm()
-        }
+    if (questionSelect === questionPack4[0] && mouseClick === "array") {
+        clearInterval(timer)
+        displayForm()
     }
 })
 
@@ -164,33 +151,34 @@ submitEl.addEventListener("click", function (event) {
 })
 
 function renderScores() {
-        var retrieveScore = JSON.parse(localStorage.getItem("score"))
-        initialsEl.remove('p')
-        scoreEl.remove('p')
-        for (let i = 0; i < retrieveScore.length; i++) {
-            var input = retrieveScore[i]
-            initialsEl = document.createElement("p")
-            initialsEl.textContent = input["name"]
-            highScores.append(initialsEl)
-            scoreEl = document.createElement("p")
-            scoreEl.textContent = input["userScore"]
-            highScores.append(scoreEl)
-            displayScoreBoard()
-        }
+    var retrieveScore = JSON.parse(localStorage.getItem("score"))
+    initialsEl.removeChild
+    scoreEl.removeChild
+    for (let i = 0; i < retrieveScore.length; i++) {
+        var input = retrieveScore[i]
+        initialsP = document.createElement("p")
+        initialsP.textContent = input["name"]
+        initialsEl.append(initialsP)
+        scoreP = document.createElement("p")
+        scoreP.textContent = input["userScore"]
+        scoreEl.append(scoreP)
+        displayScoreBoard()
     }
+}
 
 //view high score button
 highScoreBtn.addEventListener("click", function () {
     if (JSON.parse(localStorage.getItem("score") === null)) {
         alert("you need to take the quiz atleast once to view highscores!")
-    }else{
+    } else {
         displayScoreBoard()
         renderScores()
-    }})
+    }
+})
 
 function displayScoreBoard() {
     questionHolder.style.display = "none";
-    highScores.style.display = "block";
+    highScores.style.display = "flex";
     highScoreBtn.style.display = "none";
     startBtn.style.display = "none";
     quizBtn.style.display = "block";
